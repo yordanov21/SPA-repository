bd00a6e72d9992f4366bc1e6a9c882f1e916fbf5
Yordan Yordanov <y.yordanov90@gmail.com>
2020-04-28 18:42:40 +0300

    home view edit

--- SceletonExploreBulgariaBlog/scripts/app-buttons.js
+++ SceletonExploreBulgariaBlog/scripts/app-buttons.js
@@ -71,3 +71,27 @@ function loadDestination(e) {
     imageInput.value = imageDestination;
     // e.preventDefault();
 }
+
+
+function loadPicture() {
+
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $(".file-upload").on('change', function() {
+        readURL(this);
+    });
+
+    $(".upload-button").on('click', function() {
+        $(".file-upload").click();
+    });
+}
